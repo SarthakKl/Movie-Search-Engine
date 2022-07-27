20f7a3a122fda62b1b4bd7502db6a530dad0c203
SarthakKl <sarthakk60@gmail.com>
2022-07-27 19:47:11 +0530

    Final Touch

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -20,15 +20,30 @@ function Movie() {
   return (
     <div className = 'MovieParent'>
       {movie && 
-        <div>
-          <h1 style = {{textAlign:'center'}}>{movie.title}</h1>
-            <div className = 'ImageCover'></div>
-            <img 
-              className = 'Image'
-              src = {imageBaseUrl + movie.backdrop_path} 
-              alt = "Image Poster"
-            />
-              <p>{movie.overview}</p>
+        <div className='movieHeader'>
+          <h1 style = {{textAlign:'center',  color:'wheat'}}>{movie.title}</h1>
+            {/* <div className = 'ImageCover'></div> */}
+            
+            <div className = 'movieDetails'>
+              <div>
+                <p>Overview<br/>{movie.overview}</p>
+                <p style = {{marginTop:'10px'}}>
+                  <span>Genre: </span>{
+                  movie.genres.map((genre)=>{
+                    return <span>{genre.name}, </span>
+                  })}
+                </p>
+                <p style = {{marginTop:'10px'}}>
+                  <span>Release Date: </span>{movie.release_date}
+                </p>
+              </div>
+              <img 
+                className = 'movieImage'
+                src = {imageBaseUrl + movie.backdrop_path} 
+                alt = "Image Poster"
+              />
+              </div>
+              
         </div>
             
       }  
